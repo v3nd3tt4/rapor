9f5e3b66ffe0f8d7b7caab014dec7c1f01bc4823
v3nd3tt4 <pilopaokta@gmail.com>
2017-11-19 08:55:41 +0700

    config base_url

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/rapor';
+$config['base_url'] = 'http://www.proyek.ga/rapor/';
 
 /*
 |--------------------------------------------------------------------------
